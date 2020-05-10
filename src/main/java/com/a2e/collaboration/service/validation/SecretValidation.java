b13e868bf178fb0bcadc9fbbeb616aab60eb914f
tejaswini.a <tejaswini.a@turvo.com>
2020-05-10 18:01:39 +0530

    modifying OTP logic

--- src/main/java/com/a2e/collaboration/service/validation/SecretValidation.java
+++ src/main/java/com/a2e/collaboration/service/validation/SecretValidation.java
@@ -1,5 +1,6 @@
 package com.a2e.collaboration.service.validation;
 
+import com.a2e.collaboration.commons.Utilities;
 import org.springframework.stereotype.Service;
 
 import static com.a2e.collaboration.commons.A2EErrorCode.*;
@@ -8,13 +9,17 @@ import static com.a2e.collaboration.commons.A2EErrorCode.*;
 public class SecretValidation {
 
     public SecretValidation isValidOTP(String otp) throws ValidationException {
-        if(otp.length() == 0)
+        if(Utilities.isNull(otp))
+            throw new ValidationException(INVALID_OTP);
+        else if(otp.length() == 0)
             throw new ValidationException(EMPTY_OTP);
         else
             return this;
     }
     public SecretValidation isValidPassword(String password) throws ValidationException {
-        if(password.length() == 0)
+        if(Utilities.isNull(password))
+            throw new ValidationException(INVALID_PASSWORD);
+        else if(password.length() == 0)
             throw new ValidationException(EMPTY_PASSWORD);
         else
             return this;
